c442f54f55dc21a410b1e201c0ba61d8369487af
Bryan Thetford <bryan.thetford@gmail.com>
2022-12-07 10:56:02 -0600

    console math - multiplication

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Mathy
+{
+  public static void Main()
+  {
+    int num1, num2;
+     
+    Console.Write("Input the first number to multiply: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+     
+    Console.Write("Input the second number to multiply: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+     
+     
+    int result = num1 * num2;
+    Console.WriteLine("Output: {0} x {1} = {2}", 
+                        num1, num2, result);
+  }
+}
